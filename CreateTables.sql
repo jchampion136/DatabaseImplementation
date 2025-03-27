06e9453f9b08b1b8620d35e93597a2f010228755
Jackson Champion <jackson.champion136@gmail.com>
2025-03-27 18:15:41 -0400

    Changed Attribute names and added Constraints

--- CreateTables.sql
+++ CreateTables.sql
@@ -4,8 +4,8 @@
 
     CREATE TABLE CUSTOMER (
         customer_CustID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-        customer_FNAME VARCHAR(30) NOT NULL,
-        customer_LNAME VARCHAR(30) NOT NULL,
+        customer_FName VARCHAR(30) NOT NULL,
+        customer_LName VARCHAR(30) NOT NULL,
         customer_PhoneNum VARCHAR(30) NOT NULL
     );
 
@@ -21,7 +21,7 @@
         discount_DiscountID INT PRIMARY KEY AUTO_INCREMENT,
         discount_DiscountName VARCHAR(30) NOT NULL,
         discount_Amount DECIMAL(5,2),
-        discount_isPercent TINYINT NOT NULL
+        discount_IsPercent TINYINT NOT NULL
     );
 
     CREATE TABLE TOPPING (
@@ -39,13 +39,13 @@
 
     CREATE TABLE ORDERTABLE (
         ordertable_OrderID INT PRIMARY KEY AUTO_INCREMENT,
-        customer_custID INT,
+        customer_CustID INT,
         ordertable_OrderType VARCHAR(30) NOT NULL,
         ordertable_OrderDateTime DATETIME,
         ordertable_CustPrice DECIMAL(5,2) NOT NULL,
         ordertable_BusPrice DECIMAL(5,2) NOT NULL,
-        ordertable_isComplete TINYINT(1) NOT NULL,
-        FOREIGN KEY (customer_custID) REFERENCES CUSTOMER(customer_CustID)
+        ordertable_IsComplete TINYINT(1) NOT NULL,
+        CONSTRAINT ORDERTABLE_CUSTOMER_FK FOREIGN KEY (customer_CustID) REFERENCES CUSTOMER(customer_CustID)
     );
 
     CREATE TABLE PIZZA (
@@ -57,40 +57,39 @@
         pizza_CustPrice DECIMAL(5,2) NOT NULL,
         pizza_BusPrice DECIMAL(5,2) NOT NULL,
         ordertable_OrderID INT NOT NULL,
-        FOREIGN KEY (pizza_Size, pizza_CrustType) REFERENCES BASEPRICE(baseprice_Size, baseprice_CrustType),
-        FOREIGN KEY (ordertable_OrderID) REFERENCES ORDERTABLE(ordertable_OrderID)
+        CONSTRAINT PIZZA_BASEPRICE_FK FOREIGN KEY (pizza_Size, pizza_CrustType) REFERENCES BASEPRICE(baseprice_Size, baseprice_CrustType),
+        CONSTRAINT PIZZA_ORDERTABLE_FK FOREIGN KEY (ordertable_OrderID) REFERENCES ORDERTABLE(ordertable_OrderID) ON DELETE CASCADE
     );
 
-    CREATE TABLE PIZZA_TOPPING
-    (
+    CREATE TABLE PIZZA_TOPPING (
         pizza_PizzaID INT,
         topping_TopID INT,
-        pizza_topping_isDouble INT NOT NULL,
+        pizza_topping_IsDouble INT NOT NULL,
         PRIMARY KEY (pizza_PizzaID, topping_TopID),
-        FOREIGN KEY (pizza_PizzaID) REFERENCES PIZZA (pizza_PizzaID),
-        FOREIGN KEY (topping_TopID) REFERENCES TOPPING(topping_TopID)
+        CONSTRAINT PIZZA_TOPPING_PIZZA_FK FOREIGN KEY (pizza_PizzaID) REFERENCES PIZZA (pizza_PizzaID) ON DELETE CASCADE,
+        FOREIGN KEY PIZZA_TOPPING_TOPPING_FK (topping_TopID) REFERENCES TOPPING(topping_TopID)
     );
 
     CREATE TABLE PIZZA_DISCOUNT (
         pizza_PizzaID INT,
         discount_DiscountID INT,
         PRIMARY KEY (pizza_PizzaID, discount_DiscountID),
-        FOREIGN KEY (pizza_PizzaID) REFERENCES pizza(pizza_PizzaID),
-        FOREIGN KEY (discount_DiscountID) REFERENCES DISCOUNT(discount_DiscountID)
+        CONSTRAINT PIZZA_DISCOUNT_PIZZA_FK FOREIGN KEY (pizza_PizzaID) REFERENCES pizza(pizza_PizzaID) ON DELETE CASCADE,
+        CONSTRAINT PIZZA_DISCOUNT_DISCOUNT_FK FOREIGN KEY (discount_DiscountID) REFERENCES DISCOUNT(discount_DiscountID)
     );
 
     CREATE TABLE ORDER_DISCOUNT (
         ordertable_OrderID INT,
         discount_DiscountID INT,
         PRIMARY KEY (ordertable_OrderID, discount_DiscountID),
-        FOREIGN KEY (ordertable_OrderID) REFERENCES ORDERTABLE(ordertable_OrderID),
-        FOREIGN KEY (discount_DiscountID) REFERENCES DISCOUNT(discount_DiscountID)
+        CONSTRAINT ORDER_DISCOUNT_ORDERTABLE_FK FOREIGN KEY (ordertable_OrderID) REFERENCES ORDERTABLE(ordertable_OrderID) ON DELETE CASCADE,
+        CONSTRAINT ORDER_DISCOUNT_DISCOUNT_FK FOREIGN KEY (discount_DiscountID) REFERENCES DISCOUNT(discount_DiscountID)
     );
 
     CREATE TABLE PICKUP (
         ordertable_OrderID INT PRIMARY KEY,
-        pickup_isPickedUP TINYINT NOT NULL,
-        FOREIGN KEY (ordertable_OrderID) REFERENCES ORDERTABLE(ordertable_OrderID)
+        pickup_IsPickedUP TINYINT NOT NULL,
+        CONSTRAINT PICKUP_ORDERTABLE_FK FOREIGN KEY (ordertable_OrderID) REFERENCES ORDERTABLE(ordertable_OrderID) ON DELETE CASCADE
     );
 
     CREATE TABLE DELIVERY (
@@ -99,13 +98,13 @@
         delivery_Street VARCHAR(30) NOT NULL,
         delivery_City VARCHAR(30) NOT NULL,
         delivery_State VARCHAR(2) NOT NULL,
-        delivery_ZIP INT NOT NULL,
-        delivery_isDelivered TINYINT NOT NULL,
-        FOREIGN KEY (ordertable_OrderID) References ORDERTABLE(ordertable_OrderID)
+        delivery_Zip INT NOT NULL,
+        delivery_IsDelivered TINYINT NOT NULL,
+        CONSTRAINT DELIVERY_ODERTABLE_FK FOREIGN KEY (ordertable_OrderID) References ORDERTABLE(ordertable_OrderID) ON DELETE CASCADE
     );
 
     CREATE TABLE DINEIN (
         ordertable_OrderID INT PRIMARY KEY ,
         dinein_TableNum INT NOT NULL,
-        FOREIGN KEY (ordertable_OrderID) REFERENCES ORDERTABLE(ordertable_OrderID)
+        CONSTRAINT DINEIN_ORDERTABLE_FK FOREIGN KEY (ordertable_OrderID) REFERENCES ORDERTABLE(ordertable_OrderID) ON DELETE CASCADE
     );
