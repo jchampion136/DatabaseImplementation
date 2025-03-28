fed1635d13cd4479975124fde91991fbe40b75c3
Jackson Champion <jackson.champion136@gmail.com>
2025-03-28 12:10:56 -0400

    Changed dateformat so date prints without a leading 0

--- CreateViews.sql
+++ CreateViews.sql
@@ -3,7 +3,7 @@ SELECT
     pizza_Size AS Size,
     pizza_CrustType AS Crust,
     SUM(p.pizza_CustPrice - p.pizza_BusPrice) AS Profit,
-    DATE_FORMAT(ordertable_OrderDateTime, '%m/%Y') AS OrderMonth
+    DATE_FORMAT(ordertable_OrderDateTime, '%c/%Y') AS OrderMonth
 FROM pizza p
 JOIN ordertable o on p.ordertable_OrderID = o.ordertable_OrderID
 GROUP BY p.pizza_Size, p.pizza_CrustType, OrderMonth
